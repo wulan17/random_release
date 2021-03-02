3b4f45f900820fcc718c14db0a8f0a0a0540e7d2
wulan17 <galihgustip@gmail.com>
2021-03-02 23:29:48 +0700

    use own docker image
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-pacman -Syu --noconfirm
-pacman -Sy --noconfirm --needed base-devel git curl wget ccache xmlto kmod inetutils bc libelf git cpio perl tar xz zstd python python-pip pahole glibc
+#pacman -Syu --noconfirm
+pacman -Sy --noconfirm --needed base-devel git curl wget ccache xmlto kmod inetutils bc git cpio perl tar xz zstd python python-pip pahole glibc
 # use all possible cores for subsequent package builds
-sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
-sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
+#sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
+#sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
 useradd --create-home wulan17
 usermod --append --groups wheel wulan17
 echo "wulan17 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers
 cp patch-for-spflashtool.patch Allow-Disable-MSR-Lockdown.patch remove-plus-char-from-localversion.patch PKGBUILD .config .SRCINFO linux-zen-wulan17.conf linux-zen-wulan17.install linux-zen-wulan17.preset /home/wulan17/
+cp makepkg.conf /etc/
+chmod 0644 /etc/makepkg.conf
 chown -R wulan17 /home/wulan17
 cd /home/wulan17
 su wulan17 -c 'mkdir src'
