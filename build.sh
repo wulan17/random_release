b231b8916521e88befe7eca5d9d19876c9d49094
wulan17 <galihgustip@gmail.com>
2021-03-09 02:58:02 +0700

    fix build in dockerimage
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 pacman -Sy
-pacman -S --noconfirm --needed git curl wget ccache glibc
 # use all possible cores for subsequent package builds
-#sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
-#sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
-useradd --create-home wulan17
-usermod --append --groups wheel wulan17
-echo "wulan17 ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers
+sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
+sed -i 's,!ccache,ccache,g' /etc/makepkg.conf
 cp patch-for-spflashtool.patch Allow-Disable-MSR-Lockdown.patch remove-plus-char-from-localversion.patch PKGBUILD .config .SRCINFO linux-zen-wulan17.conf linux-zen-wulan17.install linux-zen-wulan17.preset /home/wulan17/
 cp makepkg.conf /etc/
 chmod 0644 /etc/makepkg.conf
@@ -19,7 +15,8 @@ su wulan17 -c 'cp .config src/build/'
 export BUILD_START=$(date +"%s")
 su wulan17 -c 'makepkg -s --noconfirm'
 export BUILD_END=$(date +"%s")
-su wulan17 -c "git clone https://wulan17:$token@github.com/wulan17/credentials.git -b up2 upload" 
+su wulan17 -c "git clone https://wulan17:$token@github.com/wulan17/credentials.git -b up2 upload"
+pacman --noconfirm -S python python-pip glibc 
 su wulan17 -c 'pip install telethon tgcrypto'
 su wulan17 -c 'pip install -r upload/requirements.txt'
 export build_time=$((BUILD_END - BUILD_START))
